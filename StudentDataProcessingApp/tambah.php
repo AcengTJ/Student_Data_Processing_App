fd5bc2225dc2a97760b798ab6a580ba0cf4e4d16
AcengTJ <acengtjhin69@gmail.com>
2023-11-28 13:46:54 +0700

    Menambahkan project student data processing app

---
+++ StudentDataProcessingApp/tambah.php
@@ -0,0 +1,72 @@
+<?php 
+	require_once("functions.php");
+
+	if (isset($_POST["submit"])) {
+		// cek apakah data berhasil ditambahkan atau tidak
+		if (tambah($_POST) > 0) {
+			echo "
+				<script>
+					alert('data berhasil ditambhakan!');
+					document.location.href = 'index.php';
+				</script>	
+			";
+		}else {
+			echo "
+				<script>
+					alert('data gagal ditambhakan!');
+					document.location.href = 'index.php';
+				</script>
+			";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tambah Data Mahasiswa</title>
+</head>
+<body>
+	<h1>Tambah Data Mahasiswa</h1>
+
+	<form action="" method="POST">
+		<ul>
+			<li>
+				<label for="nama">Nama :</label>
+				<input type="text" name="nama" id="nama" required>
+			</li>
+			<li>
+				<label for="npm">NPM :</label>
+				<input type="text" name="npm" id="npm" required>
+			</li>
+			<li>
+				<label for="email">Email :</label>
+				<input type="text" name="email" id="email" required>
+			</li>
+			<li>
+				<label for="jurusan">Jurusan :</label>
+				<input type="text" name="jurusan" id="jurusan" required>
+			</li>
+			<li>
+				<label for="gambar">Gambar :</label>
+				<input type="text" name="gambar" id="gambar">
+			</li>
+			<li>
+				<button type="submit" name="submit">Tambah Data!</button>
+			</li>
+		</ul>
+
+
+
+	</form>
+
+
+
+
+
+
+
+</body>
+</html>
